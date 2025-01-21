e88cd5595803cf35a2b8d5db11dae690841dd992
Leahcim <kelaldea27@gmail.com>
2025-01-21 14:36:30 +0800

    icon

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-
+    <link rel="icon" href="img/Header-Pic/rommel-logo-v3.svg">
     <title>Rommel | Home</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
         integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
