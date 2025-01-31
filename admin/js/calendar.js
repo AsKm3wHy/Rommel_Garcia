eb7194755b498ae236010eae782ea9655638a688
Leahcim1270 <aldeakel27@gmail.com>
2025-01-31 16:31:54 +0800

    update calendar

--- admin/js/calendar.js
+++ admin/js/calendar.js
@@ -11,15 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     events: [
       {
-        title: "All Day Event",
-        start: "2025-01-01",
-      },
-      {
-        title: "Long Event",
-        start: "2025-01-07",
-        end: "2025-01-10",
-      },
-      {
         groupId: "999",
         title: "Repeating Event",
         start: "2025-01-09T16:00:00",
@@ -29,11 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         title: "Repeating Event",
         start: "2025-01-16T16:00:00",
       },
-      {
-        title: "Conference",
-        start: "2025-01-11",
-        end: "2025-01-13",
-      },
+
       {
         title: "Meeting",
         start: "2025-01-12T10:30:00",
@@ -52,9 +39,9 @@ document.addEventListener("DOMContentLoaded", function () {
         start: "2025-01-13T07:00:00",
       },
       {
-        title: "Click for Google",
-        url: "https://google.com/",
-        start: "2025-01-28",
+        title: "Michael ",
+        url: "Appointment.php",
+        start: "2025-01-30T14:30:00",
       },
     ],
   });
